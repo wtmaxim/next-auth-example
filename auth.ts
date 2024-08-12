036d4e5a21b8aec8800067216c1fb5d3a08e73b4
wtmaxim <maxime.derame@hotmail.fr>
2024-08-12 15:22:45 +0200

    wip

--- auth.ts
+++ auth.ts
@@ -1,34 +1,35 @@
 import NextAuth from "next-auth"
 import "next-auth/jwt"
 
-import Apple from "next-auth/providers/apple"
-import Auth0 from "next-auth/providers/auth0"
-import AzureB2C from "next-auth/providers/azure-ad-b2c"
-import BankIDNorway from "next-auth/providers/bankid-no"
-import BoxyHQSAML from "next-auth/providers/boxyhq-saml"
-import Cognito from "next-auth/providers/cognito"
-import Coinbase from "next-auth/providers/coinbase"
-import Discord from "next-auth/providers/discord"
-import Dropbox from "next-auth/providers/dropbox"
-import Facebook from "next-auth/providers/facebook"
-import GitHub from "next-auth/providers/github"
-import GitLab from "next-auth/providers/gitlab"
-import Google from "next-auth/providers/google"
-import Hubspot from "next-auth/providers/hubspot"
-import Keycloak from "next-auth/providers/keycloak"
-import LinkedIn from "next-auth/providers/linkedin"
-import Netlify from "next-auth/providers/netlify"
-import Okta from "next-auth/providers/okta"
-import Passage from "next-auth/providers/passage"
-import Passkey from "next-auth/providers/passkey"
-import Pinterest from "next-auth/providers/pinterest"
-import Reddit from "next-auth/providers/reddit"
-import Slack from "next-auth/providers/slack"
-import Spotify from "next-auth/providers/spotify"
-import Twitch from "next-auth/providers/twitch"
-import Twitter from "next-auth/providers/twitter"
-import WorkOS from "next-auth/providers/workos"
-import Zoom from "next-auth/providers/zoom"
+// import Apple from "next-auth/providers/apple"
+// import Auth0 from "next-auth/providers/auth0"
+// import AzureB2C from "next-auth/providers/azure-ad-b2c"
+// import BankIDNorway from "next-auth/providers/bankid-no"
+// import BoxyHQSAML from "next-auth/providers/boxyhq-saml"
+// import Cognito from "next-auth/providers/cognito"
+// import Coinbase from "next-auth/providers/coinbase"
+// import Discord from "next-auth/providers/discord"
+// import Dropbox from "next-auth/providers/dropbox"
+// import Facebook from "next-auth/providers/facebook"
+// import GitHub from "next-auth/providers/github"
+// import GitLab from "next-auth/providers/gitlab"
+// import Google from "next-auth/providers/google"
+// import Hubspot from "next-auth/providers/hubspot"
+// import Keycloak from "next-auth/providers/keycloak"
+// import LinkedIn from "next-auth/providers/linkedin"
+// import Netlify from "next-auth/providers/netlify"
+// import Okta from "next-auth/providers/okta"
+// import Passage from "next-auth/providers/passage"
+// import Passkey from "next-auth/providers/passkey"
+// import Pinterest from "next-auth/providers/pinterest"
+// import Reddit from "next-auth/providers/reddit"
+// import Slack from "next-auth/providers/slack"
+// import Spotify from "next-auth/providers/spotify"
+// import Twitch from "next-auth/providers/twitch"
+// import Twitter from "next-auth/providers/twitter"
+// import WorkOS from "next-auth/providers/workos"
+// import Zoom from "next-auth/providers/zoom"
+import Strava from "next-auth/providers/strava"
 import { createStorage } from "unstorage"
 import memoryDriver from "unstorage/drivers/memory"
 import vercelKVDriver from "unstorage/drivers/vercel-kv"
@@ -49,52 +50,56 @@ const config = {
   theme: { logo: "https://authjs.dev/img/logo-sm.png" },
   adapter: UnstorageAdapter(storage),
   providers: [
-    Apple,
-    Auth0,
-    AzureB2C({
-      clientId: process.env.AUTH_AZURE_AD_B2C_ID,
-      clientSecret: process.env.AUTH_AZURE_AD_B2C_SECRET,
-      issuer: process.env.AUTH_AZURE_AD_B2C_ISSUER,
+    // Apple,
+    // Auth0,
+    // AzureB2C({
+    //   clientId: process.env.AUTH_AZURE_AD_B2C_ID,
+    //   clientSecret: process.env.AUTH_AZURE_AD_B2C_SECRET,
+    //   issuer: process.env.AUTH_AZURE_AD_B2C_ISSUER,
+    // }),
+    // BankIDNorway,
+    // BoxyHQSAML({
+    //   clientId: "dummy",
+    //   clientSecret: "dummy",
+    //   issuer: process.env.AUTH_BOXYHQ_SAML_ISSUER,
+    // }),
+    // Cognito,
+    // Coinbase,
+    // Discord,
+    // Dropbox,
+    // Facebook,
+    // GitHub,
+    // GitLab,
+    // Google,
+    // Hubspot,
+    // Keycloak({ name: "Keycloak (bob/bob)" }),
+    // LinkedIn,
+    // Netlify,
+    // Okta,
+    // Passkey({
+    //   formFields: {
+    //     email: {
+    //       label: "Username",
+    //       required: true,
+    //       autocomplete: "username webauthn",
+    //     },
+    //   },
+    // }),
+    // Passage,
+    // Pinterest,
+    // Reddit,
+    // Slack,
+    // Spotify,
+    Strava({
+      clientId: process.env.AUTH_STRAVA_ID,
+      clientSecret: process.env.AUTH_STRAVA_SECRET,
     }),
-    BankIDNorway,
-    BoxyHQSAML({
-      clientId: "dummy",
-      clientSecret: "dummy",
-      issuer: process.env.AUTH_BOXYHQ_SAML_ISSUER,
-    }),
-    Cognito,
-    Coinbase,
-    Discord,
-    Dropbox,
-    Facebook,
-    GitHub,
-    GitLab,
-    Google,
-    Hubspot,
-    Keycloak({ name: "Keycloak (bob/bob)" }),
-    LinkedIn,
-    Netlify,
-    Okta,
-    Passkey({
-      formFields: {
-        email: {
-          label: "Username",
-          required: true,
-          autocomplete: "username webauthn",
-        },
-      },
-    }),
-    Passage,
-    Pinterest,
-    Reddit,
-    Slack,
-    Spotify,
-    Twitch,
-    Twitter,
-    WorkOS({
-      connection: process.env.AUTH_WORKOS_CONNECTION!,
-    }),
-    Zoom,
+    // Twitch,
+    // Twitter,
+    // WorkOS({
+    //   connection: process.env.AUTH_WORKOS_CONNECTION!,
+    // }),
+    // Zoom,
   ],
   basePath: "/auth",
   callbacks: {
